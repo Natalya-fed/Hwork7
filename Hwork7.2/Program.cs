f9a08a4d26e40689b78f76c5f9503484a88b236a
Наталья Фёдорова <natalyafedorova5@gmail.com>
2021-10-24 23:13:50 +0300

    Добавьте файлы проекта.

---
+++ Hwork7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hwork7._2
+{
+    class Program
+    {
+            static void Cub(double a, out double V, out double S)
+            {
+                V = Math.Pow(a, 3);
+                S = 6 * Math.Pow(a, 2);
+                Console.WriteLine($"Площадь куба S= {S:f2}\nОбъем куба V= {V:f2}");
+            }
+            static void Main(string[] args)
+            {
+                //Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+                double a, S, V;
+                Console.Write("Введите длину ребра куба A=");
+                a = Convert.ToDouble(Console.ReadLine());
+                Cub(a, out V, out S);
+                Console.ReadKey();
+            }
+    }
+}
---
+++ Hwork7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hwork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            double P, S;
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            CalcSq(a, b, c, out S, out P);
+            double S1 = S;
+            Console.WriteLine("Введите длины сторон второго треугольника");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            CalcSq(a, b, c, out S, out P);
+            double S2 = S;
+            if (S1 > S2)
+            {
+                Console.WriteLine($"Площадь первого треугольника больше {S1:f2} > {S2:f2}");
+            }
+            else if (S1 < S2)
+            {
+                Console.WriteLine($"Площадь второго треугольника больше {S1:f2} < {S2:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"Площади первого и второго равны {S1:f2} = {S2:f2}");
+            }
+            Console.ReadKey();
+        }
+        static void CalcSq(double a, double b, double c, out double S, out double P)
+        {
+
+            P = (a + b + c) / 2;
+            S = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+        }
+    }
+    
+}
